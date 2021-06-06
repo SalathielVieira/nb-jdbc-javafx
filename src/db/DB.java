e447457c2b0254eb4fa3cb420e1bcc6e6139a94a
SalathielVieira <salatvieira@gmail.com>
2021-06-06 12:23:30 -0400

    Additional Closing Methods

--- src/db/DB.java
+++ src/db/DB.java
@@ -3,6 +3,9 @@ package db;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
 public class DB {
     
     private static Connection conn;
@@ -17,6 +20,36 @@ public class DB {
             }
         }
         return conn;
+    }
+    
+    public static void closeConecction() {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+    }
+    
+    public static void closeStatement(Statement st) {
+        if (st != null) {
+            try {
+                st.close();
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+    }
+    
+    public static void closeResultSet(ResultSet rs) {
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
     }
     
 }
